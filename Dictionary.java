ac861ab18b7016bedc2aa490042a388060a0fa00
Rivka Salem <55041326+Salem101@users.noreply.github.com>
2020-04-08 17:18:30 -0400

    Update and rename Dictionary to Dictionary.java

---
+++ Dictionary.java
@@ -0,0 +1,27 @@
+
+
+public interface Dictionary {
+
+	  Dictionary movieD = new HashTable(); 
+	  
+	  String name;
+	  String genre;
+
+	  geek.put(name, genre); 
+	  System.out.println("\nNumber genre : " + movieD.get("scifi")); 
+	  System.out.println("\nNumber genre : " + movieD.get("thriller")); 
+	  System.out.println("\nNumber genre : " + movieD.get("drama")); 
+	  System.out.println("\nNumber genre : " + movieD.get("adventure")); 
+	  System.out.println("\nNumber genre : " + movieD.get("comedy")); 
+	  System.out.println("\nNumber genre : " + movieD.get("children")); 
+	  System.out.println("\nNumber genre : " + movieD.get("crime")); 
+	  System.out.println("\nNumber genre : " + movieD.get("romance")); 
+	  System.out.println("\nNumber genre : " + movieD.get("action")); 
+	  System.out.println("\nNumber genre : " + movieD.get("horror")); 
+	  System.out.println("\nNumber genre : " + movieD.get("animation")); 
+	  System.out.println("\nNumber genre : " + movieD.get("fantasy")); 
+	  System.out.println("\nNumber genre : " + movieD.get("musical")); 
+	  System.out.println("\nNumber genre : " + movieD.get("war")); 
+	  
+	  
+}
